a44ac85995c116dbc14420bce158c8d18c05ed03
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-04-30 20:18:55 +0530

    Add files via upload

---
+++ 50th.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int i=0;
+    while(i<10001)  /*here the < symbol is used because it is increment order*/
+   
+   
+    // in while loop we only have to give contions in within the bracket not like the for loop 
+    {
+        printf("The value of i is:%d\n",i);
+
+         ++i; 
+    }
+
+}
