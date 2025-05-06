bebbf8a90deb3b3c10d861895ce4e87370c9d0af
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-05-06 20:42:25 +0530

    Add files via upload

---
+++ 56.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter the lenght of the rectangle:");
+    scanf("%d",&a);
+
+    printf("Enter the brreafth of the rectangle:");
+    scanf("%d",&b);
+
+    c=a*b;
+
+    printf("The area of the rectangle is%d",c);
+}
