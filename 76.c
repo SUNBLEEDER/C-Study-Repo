6ee1c2d6a2d8338256569b116122a9d4528583e2
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-06-06 13:14:03 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+  int i;
+  srand(time(NULL)); // Seed the random number generator with the current time
+
+  printf("10 Random Numbers between 1 and 100:\n");
+  for (i = 0; i < 10; i++) {
+    int random_number = rand() % 100 + 1; // Generates a random number between 1 and 100
+    printf("%d\n", random_number);
+  }
+  return 0;
+}
